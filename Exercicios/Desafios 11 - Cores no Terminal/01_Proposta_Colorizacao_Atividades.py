672106f7fd623fbef0400ba526f42cae145d25ed
Rodrigo Giaffredo <rodrigo.giaffredo@gmail.com>
2026-07-29 10:25:59 -0300

    Commit proposta de atividade cores no terminal
    Proposta de atividade para fixação da aprendizagem sobre utilização de cores no terminal.

---
+++ Exercicios/Desafios 11 - Cores no Terminal/01_Proposta_Colorizacao_Atividades.py
@@ -0,0 +1,7 @@
+# Nível fácil: pegar 30% dos exercícios já feitos até agora e colocar cores neles
+# onde cabível e esteticamente razoável.
+
+# Nível difícil: criar um sistema de cores para 100% dos exercícios feitos até agora.
+
+# Sinceramente, eu pulei esses desafios para focar no teste final do módulo 1 e poder
+# avançar para o módulo 2.
